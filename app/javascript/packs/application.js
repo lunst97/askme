6bc464458fdd6c5295a244a7431fdedb21a73139
LunST <lunststunt@gmail.com>
2021-04-13 02:29:15 +0300

    errors heroku fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "jquery-ujs"
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
 
 $(function (){
     $('#ask-button').click(function (){
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,24 @@
 const { environment } = require('@rails/webpacker')
-const  webpack = require('webpack')
+const merge = require('webpack-merge')
+const webpack = require('webpack')
 
-environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery'
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+        $: 'jquery',
+        JQuery: 'jquery',
+        jquery: 'jquery',
+        'window.Tether': "tether",
+        Popper: ['popper.js', 'default'], // for Bootstrap 4
     })
 )
 
-module.exports = environment
+const envConfig = module.exports = environment
+const aliasConfig = module.exports = {
+    resolve: {
+        alias: {
+            jquery: 'jquery/src/jquery'
+        }
+    }
+}
+
+module.exports = merge(envConfig.toWebpackConfig(), aliasConfig)
