e4167e7a07bc32378dd2fd3c89780ef2a489ef07
LunST <lunststunt@gmail.com>
2021-04-15 01:15:27 +0300

    add dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   attr_accessor :password
 
-  has_many :questions
+  has_many :questions, dependent: :destroy
   
   before_validation :downcase_email_username
   before_save :encrypt_password
