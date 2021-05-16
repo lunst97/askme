2bb74098f1c5db5d0ac817cc7295473c67d5db08
LunST <lunststunt@gmail.com>
2021-05-17 01:13:47 +0300

    fix logout in icon

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  resources :hashtags
   root 'users#index'
 
-  resources :users, only: [:new, :create, :update, :show, :index, :destroy, :edit]
-  resources :session, only: [:new, :create, :destroy]
+  resources :users
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
-
-  get 'sing_up' => 'users#new'
-  get 'log_out' => 'session#destroy'
-  get 'log_in' => 'session#new'
-  get 'show' => 'users#show'
+  resources :hashtags
 end
