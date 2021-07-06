6896b5341a72f21d8df947bc8fb2e30f9ce3d406
LunST <lunststunt@gmail.com>
2021-07-06 23:46:28 +0300

    refactor hashtag_controller(codestyle) | refactor views /show/hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,6 @@
 class HashtagsController < ApplicationController
-  # GET /hashtags/1
   rescue_from ActiveRecord::RecordNotFound, with: :have_not_hashtag
+
   def show
     @hashtag = Hashtag.only_have_question.find_by!(name: params[:name])
     @hashtag_questions = @hashtag.questions
